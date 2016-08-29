c11ea5a5577a06393db7bbbae0494a425752f353
Keith Salisbury <keithsalisbury@gmail.com>
2016-08-29 16:09:48 +0100

    Refactor getting key and finger event to private function

--- apps/typehero/lib/typehero/event_handler.ex
+++ apps/typehero/lib/typehero/event_handler.ex
@@ -33,34 +33,36 @@ defmodule Typehero.EventHandler do
     {:reply, state, state}
   end
 
-  def handle_cast({:receive, :key_event, key, count}, state = %Events{finger_events: finger_events}) do
-    new_state = process_key_event(Map.get(finger_events, count), state, count, key)
+  def handle_cast({:receive, :key_event, key, count}, state) do
+    finger = get_finger_event(state, count)
+    new_state = process_key(finger, state, count, key)
     {:noreply, new_state}
   end
 
-  def handle_cast({:receive, :finger_event, finger, count}, state = %Events{key_events: key_events}) do
-    new_state = process_finger_event(Map.get(key_events, count), state, count, finger)
+  def handle_cast({:receive, :finger_event, finger, count}, state) do
+    key = get_key_event(state, count)
+    new_state = process_finger(key, state, count, finger)
     {:noreply, new_state}
   end
 
   # PRIVATE
 
-  defp process_key_event(nil, events = %Events{key_events: key_events}, count, key) do
+  defp process_key(nil, events = %Events{key_events: key_events}, count, key) do
     updated_key_events_map = Map.put(key_events, count, key)
     Map.put(%Events{}, :key_events, updated_key_events_map)
   end
 
-  defp process_key_event(finger, struct = %Events{}, count, key) do
+  defp process_key(finger, struct = %Events{}, count, key) do
     match_result = KeyFingerMatch.match_key_to_finger(finger, String.to_atom(key))
     correct_key_finger(match_result, key, count, struct)
   end
 
-  defp process_finger_event(nil, events = %Events{finger_events: finger_events}, count, finger) do
+  defp process_finger(nil, events = %Events{finger_events: finger_events}, count, finger) do
     updated_finger_events_map = Map.put(finger_events, count, finger)
     Map.put(%Events{}, :finger_events, updated_finger_events_map)
   end
 
-  defp process_finger_event(key, events = %Events{}, count, finger) do
+  defp process_finger(key, events = %Events{}, count, finger) do
     match_result = KeyFingerMatch.match_key_to_finger(finger, String.to_atom(key))
     correct_key_finger(match_result, key, count, events)
   end
@@ -98,5 +100,13 @@ defmodule Typehero.EventHandler do
     %{events | key_events: Map.delete(events.key_events, count),
       finger_events: Map.delete(events.finger_events, count)
      }
+  end
+
+  defp get_finger_event(%Events{finger_events: finger_events}, count) do
+    Map.get(finger_events, count)
+  end
+
+  defp get_key_event(%Events{key_events: key_events}, count) do
+    Map.get(key_events, count)
   end
 end
