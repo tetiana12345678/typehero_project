e9eaf1c05e6291e2e50899e317fdb3a208659a7a
Keith Salisbury <keithsalisbury@gmail.com>
2016-08-29 16:13:32 +0100

    Pattern match get_event

--- apps/typehero/lib/typehero/event_handler.ex
+++ apps/typehero/lib/typehero/event_handler.ex
@@ -10,12 +10,12 @@ defmodule Typehero.EventHandler do
     GenServer.start_link(__MODULE__, %Events{}, name: :event_handler)
   end
 
-  def key_event(key, count) do
-    GenServer.cast(:event_handler, {:receive, :key_event, key, count})
+  def key_event(key, id) do
+    GenServer.cast(:event_handler, {:receive, :key_event, key, id})
   end
 
-  def finger_event(finger, count) do
-    GenServer.cast(:event_handler, {:receive, :finger_event, finger, count})
+  def finger_event(finger, id) do
+    GenServer.cast(:event_handler, {:receive, :finger_event, finger, id})
   end
 
   def get_state do
@@ -33,80 +33,80 @@ defmodule Typehero.EventHandler do
     {:reply, state, state}
   end
 
-  def handle_cast({:receive, :key_event, key, count}, state) do
-    finger = get_finger_event(state, count)
-    new_state = process_key(finger, state, count, key)
+  def handle_cast({:receive, :key_event, key, id}, state) do
+    finger = get_event(:finger, state, id)
+    new_state = process_key(finger, state, id, key)
     {:noreply, new_state}
   end
 
-  def handle_cast({:receive, :finger_event, finger, count}, state) do
-    key = get_key_event(state, count)
-    new_state = process_finger(key, state, count, finger)
+  def handle_cast({:receive, :finger_event, finger, id}, state) do
+    key = get_event(:key, state, id)
+    new_state = process_finger(key, state, id, finger)
     {:noreply, new_state}
   end
 
   # PRIVATE
 
-  defp process_key(nil, events = %Events{key_events: key_events}, count, key) do
-    updated_key_events_map = Map.put(key_events, count, key)
+  defp process_key(nil, events = %Events{key_events: key_events}, id, key) do
+    updated_key_events_map = Map.put(key_events, id, key)
     Map.put(%Events{}, :key_events, updated_key_events_map)
   end
 
-  defp process_key(finger, struct = %Events{}, count, key) do
+  defp process_key(finger, struct = %Events{}, id, key) do
     match_result = KeyFingerMatch.match_key_to_finger(finger, String.to_atom(key))
-    correct_key_finger(match_result, key, count, struct)
+    correct_key_finger(match_result, key, id, struct)
   end
 
-  defp process_finger(nil, events = %Events{finger_events: finger_events}, count, finger) do
-    updated_finger_events_map = Map.put(finger_events, count, finger)
+  defp process_finger(nil, events = %Events{finger_events: finger_events}, id, finger) do
+    updated_finger_events_map = Map.put(finger_events, id, finger)
     Map.put(%Events{}, :finger_events, updated_finger_events_map)
   end
 
-  defp process_finger(key, events = %Events{}, count, finger) do
+  defp process_finger(key, events = %Events{}, id, finger) do
     match_result = KeyFingerMatch.match_key_to_finger(finger, String.to_atom(key))
-    correct_key_finger(match_result, key, count, events)
+    correct_key_finger(match_result, key, id, events)
   end
 
-  defp correct_key_finger(:match = result, key, count, events) do
-    correct_key_letter(Core.get_current_letter == key, result, count, events)
+  defp correct_key_finger(:match = result, key, id, events) do
+    correct_key_letter(Core.get_current_letter == key, result, id, events)
   end
 
-  defp correct_key_finger(:dismatch = result, key, count, events) do
-    correct_key_letter(Core.get_current_letter == key, result, count, events)
+  defp correct_key_finger(:dismatch = result, key, id, events) do
+    correct_key_letter(Core.get_current_letter == key, result, id, events)
   end
 
-  defp correct_key_letter(true, :match, count, events) do
+  defp correct_key_letter(true, :match, id, events) do
     Core.update_text
-    Core.notify_web(%{result: :all_match, count: count})
-    delete_event(events, count)
+    Core.notify_web(%{result: :all_match, id: id})
+    delete_event(events, id)
   end
 
-  defp correct_key_letter(true, :dismatch, count, events) do
-    Core.notify_web(%{result: :letter_key, count: count})
-    delete_event(events, count)
+  defp correct_key_letter(true, :dismatch, id, events) do
+    Core.notify_web(%{result: :letter_key, id: id})
+    delete_event(events, id)
   end
 
-  defp correct_key_letter(false, :match, count, events) do
-    Core.notify_web(%{result: :finger_key, count: count})
-    delete_event(events, count)
+  defp correct_key_letter(false, :match, id, events) do
+    Core.notify_web(%{result: :finger_key, id: id})
+    delete_event(events, id)
   end
 
-  defp correct_key_letter(false, :dismatch, count, events) do
-    Core.notify_web(%{result: :nothing_match, count: count})
-    delete_event(events, count)
+  defp correct_key_letter(false, :dismatch, id, events) do
+    Core.notify_web(%{result: :nothing_match, id: id})
+    delete_event(events, id)
   end
 
-  defp delete_event(events, count) do
-    %{events | key_events: Map.delete(events.key_events, count),
-      finger_events: Map.delete(events.finger_events, count)
+  defp delete_event(events, id) do
+    %{events | key_events: Map.delete(events.key_events, id),
+      finger_events: Map.delete(events.finger_events, id)
      }
   end
 
-  defp get_finger_event(%Events{finger_events: finger_events}, count) do
-    Map.get(finger_events, count)
+  defp get_event(:finger, %Events{finger_events: finger_events}, id) do
+    Map.get(finger_events, id)
   end
 
-  defp get_key_event(%Events{key_events: key_events}, count) do
-    Map.get(key_events, count)
+  defp get_event(:key, %Events{key_events: key_events}, id) do
+    Map.get(key_events, id)
   end
 end
