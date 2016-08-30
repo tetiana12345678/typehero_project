32bb29564d26ae4d7c97815e2edfebcc81d88840
Keith Salisbury <keithsalisbury@gmail.com>
2016-08-30 02:12:17 +0100

    upgrade version 1.3 to support String.myerswhatever

--- mix.exs
+++ mix.exs
@@ -3,6 +3,7 @@ defmodule TypeheroProject.Mixfile do
 
   def project do
     [apps_path: "apps",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
