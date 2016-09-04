aa52920ff65e4c7ec8e78779194a86428a9bbe5b
Tetiana <tanechka289@gmail.com>
2016-09-04 20:52:44 +0100

    Update pressure_sensor.ino

--- apps/typehero_serial/pressure_sensor_arduino/pressure_sensor.ino
+++ apps/typehero_serial/pressure_sensor_arduino/pressure_sensor.ino
@@ -2,14 +2,23 @@
 const int analogPin = A0;//index finger
 const int analogPin1 = A1; // middle finger
 const int analogPin2 = A2; //Fourth finger
+const int analogPin3 = A3; //Pinkie finger
 
-int inputValue = 0;//variable to store the value coming from sensor A0
-int inputValue1 = 0; //variable to store value from sensor A1
-int inputValue2 = 0;
+int finger1 = 0; //variable to store the value coming from sensor A0
+int finger2 = 0; //variable to store value from sensor A1
+int finger3 = 0;
+int finger4 = 0;
 
-bool wasDown = false;
-bool wasDown1 = false;
-bool wasDown2 = false;
+bool finger1_down = false;
+bool finger2_down = false;
+bool finger3_down = false;
+bool finger4_down = false;
+
+int finger1_sense = 200;
+int finger2_sense = 800;
+int finger3_sense = 900;
+int finger4_sense = 800;
+
 /******************************************/
 void setup()
 {
@@ -17,44 +26,49 @@ void setup()
 }
 /******************************************/
 void loop()
+
 {
-  inputValue = analogRead(analogPin);//read the value from the sensor
 
-  if (inputValue > 100 && !wasDown) {
-    wasDown = true;
-    Serial.println("1"); //Index finger
-  } else if (inputValue <= 100) {
-    wasDown = false;
+  // FINGER 1
+  finger1 = analogRead(analogPin);//read the value from the sensor
+  if (finger1 > finger1_sense && !finger1_down) {
+    finger1_down = true;
+    Serial.print("1");
+  } else if (finger1 <= finger1_sense) {
+    finger1_down = false;
   }
 
-  inputValue1 = analogRead(analogPin1);
+  // FINGER 2
+  finger2 = analogRead(analogPin1);
 
-  if (inputValue1 > 600 && !wasDown1) {
-    wasDown1 = true;
-    Serial.println("2"); //Middle finger
-  } else if (inputValue1 <= 600) {
-    wasDown1 = false;
+  if (finger2 > finger2_sense && !finger2_down) {
+    finger2_down = true;
+    Serial.print("2");
+  } else if (finger2 <= finger2_sense) {
+    finger2_down = false;
+  }
+//
+  // FINGER 3
+  finger3 = analogRead(analogPin2);
+
+  if (finger3 > finger3_sense && !finger3_down) {
+    finger3_down = true;
+    Serial.print("3");
+  } else if (finger3 <= finger3_sense) {
+    finger3_down = false;
+  }
+//
+  // FINGER 4
+  finger4 = analogRead(analogPin3);
+
+  if (finger4 > finger4_sense && !finger4_down) {
+    finger4_down = true;
+    Serial.print("4");
+  } else if (finger4 <= finger4_sense) {
+    finger4_down = false;
   }
 
-  inputValue2 = analogRead(analogPin2);
-
-  if (inputValue2 > 100 && !wasDown2) {
-    wasDown2 = true;
-    Serial.println("3"); // Fourth finger
-  } else if (inputValue2 <= 100) {
-    wasDown2 = false;
-  }
-
-
-  //Serial.println(inputValue);
-  //Serial.print("   ");
- // Serial.println(inputValue1);
-  //Serial.print("   ");
-  //Serial.print(inputValue2);
-  //Serial.println();
-
-  // delay 10 milliseconds before the next reading:
-  // delay(10);
+  delay(100);
 
 }
 /*******************************************/
